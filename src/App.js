c10079eba545de2e609495a469c6f69c838cf825
Jacqueline Nascimento <jacqueline.cmessias@gmail.com>
2022-02-01 12:43:59 -0300

    primeiras alterações

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import { AppContainer } from './components/AppContainer'
+import { ThemeProvider } from '@material-ui/core/styles'
+import {theme} from './constants/theme'
+import Button from '@material-ui/core/Button';
+import { render } from '@testing-library/react';
+import Home from './pages/Home';
+import Contratar from './pages/Contratar';
+import Cadastro from './pages/Cadastro';
+import Carrinho from './pages/Carrinho';
+import Detalhes from './pages/Detalhes';
 
-function App() {
-	return (
-        <AppContainer />
-	)
+//Para instalar o Desing Material: npm install @material-ui/core
+//Para instalar o Desing Material: npm install @material-ui/icons
+
+class App extends React.Component {
+	state = {
+		page: 'home'
+	}
+
+
+	render(){
+		const pagina = () => {
+			switch(this.state.page){
+				case 'home':
+					return <Home />
+				case 'contratar':
+					return <Contratar />
+				case 'cadastro':
+					return <Cadastro />
+				case 'carrinho':
+					return <Carrinho />
+				case 'detalhes':
+					return <Detalhes />
+				default:
+					return <Home />
+			}
+		}
+
+		return (
+		<ThemeProvider theme={theme}>
+		<AppContainer />
+		{pagina()}
+		</ThemeProvider>
+			
+		)}
 }
 
 export default App
---
+++ src/pages/Carrinho.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Carrinho extends Component {
+  render() {
+    return <div></div>;
+  }
+}
