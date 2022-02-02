910c2a430238bbe61fe2e470f112fa9d1b8f7579
Diego Conte <diegocomte@gmail.com>
2022-02-02 09:06:55 -0300

    Layout filtros e cards page contratar

--- src/pages/Contratar.js
+++ src/pages/Contratar.js
@@ -1,7 +1,53 @@
 import React, { Component } from 'react';
+import BasicSelect, { BuscaPorTermos, BuscaValorMaximo, BuscaValorMinimo } from '../components/PageContratarLayoutGeral'
+import { ContainerFiltros, ContainerBuscaValores, ContainerCards } from '../components/PageContratarLayoutGeral';
 
 export default class Contratar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <ContainerFiltros>
+          <ContainerBuscaValores>
+            <BuscaValorMinimo />
+            <BuscaValorMaximo />
+          </ContainerBuscaValores>
+          <BuscaPorTermos />
+          <BasicSelect />
+        </ContainerFiltros>
+
+        <ContainerCards>
+          <p>
+            Título Card1 <br></br>
+            Descrição<br></br>
+            Preço<br></br>
+            Prazo<br></br>
+            Pagamento
+          </p>
+          <p>
+            Título Card2 <br></br>
+            Descrição<br></br>
+            Preço<br></br>
+            Prazo<br></br>
+            Pagamento
+          </p>
+          <p>
+            Título Card3 <br></br>
+            Descrição<br></br>
+            Preço<br></br>
+            Prazo<br></br>
+            Pagamento
+          </p>
+          <p>
+            Título Card4 <br></br>
+            Descrição<br></br>
+            Preço<br></br>
+            Prazo<br></br>
+            Pagamento
+          </p>
+
+        </ContainerCards>
+
+      </div>
+    )
   }
 }
